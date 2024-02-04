05d845cddc52f5fb3e8d46fcc026362d06cc583d
Bak <95734631+BAKHONGMIN@users.noreply.github.com>
2024-02-05 08:52:42 +0900

    톱 페이지 작성하기
    톱 페이지 작성하기

---
+++ src/containers/SigninFormContainer.tsx
@@ -0,0 +1,39 @@
+import SigninForm from "@/components/organisms/SigninForm"
+import { useAuthContext } from "@/contexts/AuthContext"
+import { useGlobalSpinnerActionContext } from "@/contexts/GlobalSpinnerContext"
+
+interface SigninFormContainerProps {
+  /**
+   * 로그인했을 때 호출되는 이벤트 핸들러
+   */
+  onSignin: (error?: Error) => void
+}
+
+/**
+ * 로그인 폼 컨테이너
+ */
+const SigninFormContainer = ({ onSignin }: SigninFormContainerProps) => {
+  const { signin } = useAuthContext()
+  const setGlobalSpinner = useGlobalSpinnerActionContext()
+
+  // 로그인 버튼을 눌렀을 때의 이벤트 핸들러
+  const handleSignin = async (username: string, password: string) => {
+    try {
+      //로딩 스피너를 표시한다.
+      setGlobalSpinner(true)
+      await signin(username, password)
+      onSignin && onSignin()
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        //에러내용을 표시한다.
+        window.alert(err.message)
+        onSignin && onSignin(err)
+      }
+    } finally {
+      setGlobalSpinner(false)
+    }
+  }
+  return <SigninForm onSignin={handleSignin}></SigninForm>
+}
+
+export default SigninFormContainer
---
+++ src/pages/signin.tsx
@@ -0,0 +1,56 @@
+import type { NextPage } from "next"
+import { useRouter } from "next/router"
+import AppLogo from "@/components/atoms/AppLogo"
+import Box from "@/components/layout/Box"
+import Flex from "@/components/layout/Flex"
+import Layout from "@/components/templates/Layout"
+import SigninFormContainer from "@/containers/SigninFormContainer"
+
+const SigninPage: NextPage = () => {
+  const router = useRouter()
+
+  //인증 후의 이벤트 핸들러
+  const handlesignin = async (err?: Error) => {
+    if (!err) {
+      //로그인에 성공하고, 쿼리가 지정돼 있을 때는 해당 URL로 이동한다.
+      // 기본은 톱 페이지로 이동한다
+      const redurectTo = (router.query["redirect_to"] as string) ?? "/"
+
+      console.log("Redirecting", redurectTo)
+      await router.push(redurectTo)
+    }
+  }
+
+  return (
+    <Layout>
+      <Flex
+        paddingTop={2}
+        paddingBottom={2}
+        paddingLeft={{ base: 2, md: 0 }}
+        paddingRight={{ base: 2, md: 0 }}
+        justifyContent="center"
+      >
+        <Flex
+          width="400px"
+          flexDirection="column"
+          justifyContent="center"
+          alignItem="center"
+        >
+          <Box marginBottom={2}>
+            <AppLogo />
+          </Box>
+          <Box width="100%">
+            {/* 
+              로그인 폼 컨테이너
+              SigninForm의 사용자명/비밀번호로부터 인증 Api를 호출하고
+              onSignin 콜백이 호출된다.
+             */}
+            <SigninFormContainer onSignin={handlesignin} />
+          </Box>
+        </Flex>
+      </Flex>
+    </Layout>
+  )
+}
+
+export default SigninPage
